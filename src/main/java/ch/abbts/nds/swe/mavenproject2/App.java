222bb27e10a1de3d0017280d7b90979f0a3e5260
anawas <aw@andreas-wassmer.ch>
2019-02-06 22:27:44 +0100

    First commit

---
+++ src/main/java/ch/abbts/nds/swe/mavenproject2/App.java
@@ -0,0 +1,13 @@
+package ch.abbts.nds.swe.mavenproject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
